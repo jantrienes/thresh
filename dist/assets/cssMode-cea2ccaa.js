5e73744fd669049ae89a701b95110891ea1cd9bb
Jan Trienes <jan.trienes@uni-due.de>
2023-09-18 17:46:26 -0500

    deploy

--- dist/assets/cssMode-cea2ccaa.js
+++ dist/assets/cssMode-cea2ccaa.js
@@ -1,4 +1,4 @@
-var Le=Object.defineProperty;var je=(e,n,i)=>n in e?Le(e,n,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[n]=i;var k=(e,n,i)=>(je(e,typeof n!="symbol"?n+"":n,i),i);import{m as Ne}from"./Builder-24a30639.js";import"./index-4cd82c34.js";import"./js-yaml-327a1ea4.js";/*!-----------------------------------------------------------------------------
+var Le=Object.defineProperty;var je=(e,n,i)=>n in e?Le(e,n,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[n]=i;var k=(e,n,i)=>(je(e,typeof n!="symbol"?n+"":n,i),i);import{m as Ne}from"./Builder-2be52ff4.js";import"./index-e5368a27.js";import"./js-yaml-76fb679f.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
  * Released under the MIT license
