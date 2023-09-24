a440848307abe6a4ea34799b4da1427344b26944
Jan Trienes <jan.trienes@uni-due.de>
2023-09-23 21:12:21 -0500

    deploy

--- dist/assets/htmlMode-3470a11f.js
+++ dist/assets/htmlMode-3470a11f.js
@@ -1,4 +1,4 @@
-var $e=Object.defineProperty;var qe=(e,n,i)=>n in e?$e(e,n,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[n]=i;var k=(e,n,i)=>(qe(e,typeof n!="symbol"?n+"":n,i),i);import{m as Qe}from"./Builder-564ca70d.js";import"./index-cd84a04b.js";import"./js-yaml-3cadfbed.js";/*!-----------------------------------------------------------------------------
+var $e=Object.defineProperty;var qe=(e,n,i)=>n in e?$e(e,n,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[n]=i;var k=(e,n,i)=>(qe(e,typeof n!="symbol"?n+"":n,i),i);import{m as Qe}from"./Builder-e16c9c39.js";import"./index-d94a5014.js";import"./js-yaml-0385108d.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
  * Released under the MIT license
