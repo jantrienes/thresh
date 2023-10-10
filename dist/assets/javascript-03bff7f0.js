136d157cadd59e4c9d1d48486de057d58d877b95
Jan Trienes <jan.trienes@uni-due.de>
2023-10-10 13:22:39 -0500

    deploy

--- dist/assets/javascript-03bff7f0.js
+++ dist/assets/javascript-03bff7f0.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-67afcc44.js";import"./Builder-04fd36a0.js";import"./index-d7a38b91.js";import"./js-yaml-ec2dff90.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-da9ccc1f.js";import"./Builder-63b5ba46.js";import"./index-151cc104.js";import"./js-yaml-54fa434d.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
  * Released under the MIT license
